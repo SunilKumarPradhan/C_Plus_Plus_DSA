911b4902981e5dde40fb571e700983707d23317c
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-06-24 09:28:08 +0530

    Added Sliding Window Problems

---
+++ 05. Arrays/00 Sliding Window Approach/AutoGenFiles.py
@@ -0,0 +1,23 @@
+import os
+
+cpp_template = """#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+"""
+
+# Read the filenames from the text file
+with open(r'C:\Users\sunil\Desktop\GFG\05. Arrays\00 Sliding Window Approach\QuestionList.txt', 'r') as file:
+    filenames = file.readlines()
+
+# Create .cpp files with the template
+for name in filenames:
+    filename = name.strip() + '.cpp'  # Remove any leading/trailing whitespace and add .cpp extension
+    filepath = os.path.join(r'C:\Users\sunil\Desktop\GFG\05. Arrays\00 Sliding Window Approach', filename)
+    with open(filepath, 'w') as cpp_file:
+        cpp_file.write(cpp_template)
+    
+    print(f"Created file: {filepath}")
