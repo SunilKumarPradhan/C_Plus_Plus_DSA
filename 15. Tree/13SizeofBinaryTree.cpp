d6c67bd4a95d5505ed1415c1b43f366811d499ab
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-07-15 22:21:26 +0530

    Header file waala idea fuddu thha

--- 15. Tree/13SizeofBinaryTree.cpp
+++ 15. Tree/13SizeofBinaryTree.cpp
@@ -1,15 +1,29 @@
-#include <bits/stdc++.h>
-#include "tree.h"
+#include <iostream>
 using namespace std;
 
-int size(node *root){
-    if(root==NULL) return 0;
-    else return 1+size(root->left)+size(root->right);
+struct node {
+    int key;
+    node *left;
+    node *right;
+
+    node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+
+int size(node *root) {
+    if (root == NULL) return 0;
+    else return 1 + size(root->left) + size(root->right);
 }
 
 int main() {
-    node *root = makeTree();
-    cout <<"The Size of the tree is : "<<size(root)<<endl;
-    
+    node *root = new node(3);
+    root->left = new node(2);
+    root->right = new node(4);
+    root->right->right = new node(5);
+    root->left->left = new node(1);
+    cout << "The Size of the tree is : " << size(root) << endl;
     return 0;
 }
