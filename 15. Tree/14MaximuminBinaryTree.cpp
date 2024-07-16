6854ce9a1f4cda9dc7a68e0636ecef1a22cca803
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-07-16 07:09:50 +0530

    Added images for trees and did few more  questions of trees .

--- 15. Tree/14MaximuminBinaryTree.cpp
+++ 15. Tree/14MaximuminBinaryTree.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct node {
+    int key;
+    node *left;
+    node *right;
+
+    node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int getMax(node *root){
+    if(root==NULL) return INT_MIN;
+    else return max( root->key ,max( getMax(root->left), getMax(root->right) ) );
+}
+
 int main() {
-    // Your code here
+    node *root = new node(3);
+    root->left = new node(2);
+    root->right = new node(4);
+    root->right->right = new node(5);
+    root->left->left = new node(1);
+    cout << "The MaxNode of the tree is : " << getMax(root) << endl;
     return 0;
 }
--- 15. Tree/15PrintLeftViewofBinaryTree.cpp
+++ 15. Tree/15PrintLeftViewofBinaryTree.cpp
@@ -1,7 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct node {
+    int key;
+    node *left;
+    node *right;
+
+    node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+// THIS IS THE RECURSIVE APPROACH--------
+
+// int maxLevel=0;
+// void leftView(node *root,int level){
+//     if(root==NULL) return;
+//     if(maxLevel <level){
+//         cout<<root->key <<" ";
+//         maxLevel=level;
+//     }
+//     leftView(root->left , level+1);
+//     leftView(root->right , level+1);
+// }
+
+// void printLeftView(node *root ){
+//     leftView(root,1);
+// }
+
+// THIS IS THE ITERATIVE APPROACH -----
+void printLeftView(node *root) {
+    if (root == NULL) return;
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int count = q.size();
+
+        for (int i = 0; i < count; i++) {
+            node *curr = q.front();
+            q.pop();
+            
+            if (i == 0) cout << curr->key << " ";
+            if (curr->left != NULL) q.push(curr->left);
+            if (curr->right != NULL) q.push(curr->right);
+        }
+    }
+}
+
 int main() {
-    // Your code here
+    node *root = new node(3);
+    root->left = new node(2);
+    root->right = new node(4);
+    root->right->right = new node(5);
+    root->left->left = new node(1);
+    cout << "The LeftView of the tree is : " << endl;
+    printLeftView(root);
     return 0;
 }
