81827fcf841cad3b94a11d1d8131157139b4e180
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-07-25 23:05:48 +0530

    Added Template for all the codes in the Graph

--- 18. Graph/04AdjacencyListImplementationInCPP.cpp
+++ 18. Graph/04AdjacencyListImplementationInCPP.cpp
@@ -1,37 +1,37 @@
-// Most easiest way of adjacency list
-
-// int main() {
-//     // Number of vertices
-//     int V = 5;
-    
-//     // Adjacency list representation using initializer lists
-//     vector<vector<int>> adjList = {
-//         {1, 2},       // Neighbors of vertex 0
-//         {0, 2},       // Neighbors of vertex 1
-//         {0, 1, 3},    // Neighbors of vertex 2
-//         {2, 4},       // Neighbors of vertex 3
-//         {3}           // Neighbors of vertex 4
-//     };
-
-//     // Printing the adjacency list
-//     cout << "Adjacency List Representation:\n";
-//     for (int i = 0; i < V; ++i) {
-//         cout << i << ": ";
-//         for (int j : adjList[i]) {
-//             cout << j << " ";
-//         }
-//         cout << endl;
-//     }
-
-//     return 0;
-// }⁡
-
-// Another way of representation as it goes , this follows like python:: 
-
 #include <bits/stdc++.h>
 using namespace std;
 
+void addEdge(vector<int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);  // Remove this line if the graph is directed
+}
+
+
+
+
 int main() {
+    int size = 5;  // Define the correct size of the graph
+    vector<int> adj[size];
+
+    addEdge(adj, 2, 3);
+    addEdge(adj, 2, 4);
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 0);
+
+    for (int i = 0; i < size; ++i) {
+        cout << i << ": ";
+        for (auto j : adj[i])
+            cout << j << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// Another way of representation as it goes , this follows like python:: 
+
+/*
    
     int V = 5;
 
@@ -71,3 +71,36 @@ int main() {
 
     return 0;
 }
+
+
+*/
+
+// Most easiest way of adjacency list
+
+/*
+int main() {
+    // Number of vertices
+    int V = 5;
+    
+    // Adjacency list representation using initializer lists
+    vector<vector<int>> adjList = {
+        {1, 2},       // Neighbors of vertex 0
+        {0, 2},       // Neighbors of vertex 1
+        {0, 1, 3},    // Neighbors of vertex 2
+        {2, 4},       // Neighbors of vertex 3
+        {3}           // Neighbors of vertex 4
+    };
+
+    // Printing the adjacency list
+    cout << "Adjacency List Representation:\n";
+    for (int i = 0; i < V; ++i) {
+        cout << i << ": ";
+        for (int j : adjList[i]) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}⁡
+*/
--- 18. Graph/06BreadthFirstSearch.cpp
+++ 18. Graph/06BreadthFirstSearch.cpp
@@ -1,7 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void addEdge(vector<int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);  // Remove this line if the graph is directed
+}
+
+void BFS(vector<int> adj[], int start, int size) {
+    vector<bool> visited(size, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int vertex = q.front();
+        cout << vertex << " ";
+        q.pop();
+
+        for (auto i : adj[vertex]) {
+            if (!visited[i]) {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+}
+
+
 int main() {
-    // Your code here
+    int size = 5;  // Define the correct size of the graph
+    vector<int> adj[size];
+
+    addEdge(adj, 2, 3);
+    addEdge(adj, 2, 4);
+    addEdge(adj, 0, 1);
+    addEdge(adj, 1, 0);
+
+    for (int i = 0; i < size; ++i) {
+        cout << i << ": ";
+        for (auto j : adj[i])
+            cout << j << " ";
+        cout << endl;
+    }
+
+    cout << "\nBFS starting from vertex 2:\n";
+    BFS(adj, 2, size);
+
+
     return 0;
 }
